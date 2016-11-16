5a6b0f8b01d49e0d99677d830374f98ac6280ea6
Nick Torres <nicos11@comcast.net>
2016-11-15 20:43:02 -0500

    implement new layouts with GUI

--- src/edu/bsu/cs376/ApplicationGUI.java
+++ src/edu/bsu/cs376/ApplicationGUI.java
@@ -5,16 +5,21 @@ import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ApplicationGUI extends Application {
     private Stage window;
     private MainMenu mainMenuPane = new MainMenu();
     private BorderPane FCFSPane = new BorderPane();
-    private BorderPane center = new BorderPane();
+    private BorderPane mainLayout = new BorderPane();
     private FCFSLayout fcfsLayout = new FCFSLayout();
+    private SJFLayout sjfLayout = new SJFLayout();
+    private RoundRobinLayout roundRobin = new RoundRobinLayout();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -54,28 +59,64 @@ public class ApplicationGUI extends Application {
     }
 
     private BorderPane FCFSLayout() {
+        FCFSPane = new BorderPane();
+        mainLayout = new BorderPane();
+        setTop("First Come First Serve");
         FCFSPane = fcfsLayout.setMainWindow();
-        //center = fcfsLayout.makeSceneInstance();
-        //FCFSPane.setBottom(center);
-        return FCFSPane;
+        mainLayout.setCenter(FCFSPane);
+        return mainLayout;
     }
 
     private Button RRButton() {
         Button RR = new Button("Round Robin");
         RR.setCursor(Cursor.HAND);
-        //RR.setOnAction(event -> {
-
-        //});
+        RR.setOnAction(event -> window.setScene(roundRobinInput()));
         return RR;
+    }
+
+    private Scene roundRobinInput(){
+        return new Scene(RRLayout(), 600, 650);
+    }
+
+    private BorderPane RRLayout() {
+        FCFSPane = new BorderPane();
+        mainLayout = new BorderPane();
+        setTop("Round Robin");
+        FCFSPane = roundRobin.setMainWindow();
+        mainLayout.setCenter(FCFSPane);
+        return mainLayout;
     }
 
     private Button SJFButton(){
         Button SJF = new Button("Shortest Job First");
         SJF.setCursor(Cursor.HAND);
-        //goToCalculator.setOnAction(event -> {
-
-        //});
+        SJF.setOnAction(event -> window.setScene(SJFInput()));
         return SJF;
+    }
+
+    private Scene SJFInput(){
+        return new Scene(SJFLayout(), 600, 650);
+    }
+
+    private BorderPane SJFLayout() {
+        FCFSPane = new BorderPane();
+        mainLayout = new BorderPane();
+        setTop("Shortest Job First");
+        FCFSPane = sjfLayout.setMainWindow();
+        mainLayout.setCenter(FCFSPane);
+        return mainLayout;
+    }
+
+    private void setTop(String selected){
+        HBox top = new HBox();
+        Label algorithm = new Label(selected);
+        algorithm.setFont(Font.font(16));
+        Button restart = new Button();
+        restart.setText("Main Menu");
+        restart.setOnAction(event->window.setScene(makeFirstSceneLayout()) );
+        top.getChildren().addAll(restart,algorithm);
+        top.setSpacing(150);
+        mainLayout.setTop(top);
     }
 
     public static void main(String[] args) {
