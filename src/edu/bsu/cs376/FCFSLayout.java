2f1df8e738782d52092733ad834cb211fd9b8aec
Nick Torres <nicos11@comcast.net>
2016-11-15 20:45:44 -0500

    add restart functionality

--- src/edu/bsu/cs376/FCFSLayout.java
+++ src/edu/bsu/cs376/FCFSLayout.java
@@ -3,28 +3,34 @@ package edu.bsu.cs376;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.util.ArrayList;
 
 public class FCFSLayout {
 
-    private BorderPane mainLayout = new BorderPane();
+    protected BorderPane mainLayout = new BorderPane();
     private BorderPane top = new BorderPane();
+    private ComboBox jobBox;
+    ArrayList<TextArea> times = new ArrayList();
+    private Button calculate = new Button();
 
     public BorderPane setMainWindow(){
+        mainLayout.setCenter(null);
+        mainLayout.setBottom(null);
         mainLayout.setTop(top());
         return mainLayout;
     }
 
     private BorderPane top(){
         top.setTop(jobPrompt());
-        top.setCenter(makeDropdown());
+        top.setCenter(calculateButton());
+        top.setBottom(makeDropdown());
         return top;
     }
 
@@ -35,20 +41,81 @@ public class FCFSLayout {
         return promptNumber;
     }
 
+    private Button calculateButton(){
+        calculate.setText("Calculate");
+        calculate.setOnAction(event -> {
+            if(hasInvalidInput()){
+                mainLayout.setBottom(error());
+            }
+            else {
+                simulate();
+            }
+        });
+        calculate.setVisible(false);
+        return calculate;
+    }
+
+    public void simulate(){
+        String output = "";
+        for(int i = 0; i < times.size(); i++){
+            output +="  Job " + (i+1) + "," + times.get(i).getText() + ", ";
+        }
+        showOutput(output);
+    }
+
+    protected boolean hasInvalidInput(){
+        for (TextArea time : times) {
+            if (!time.getText().matches("[0-9]+")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void showOutput(String output){
+        VBox bottom = new VBox();
+        Label out = new Label();
+        out.setText(output);
+        out.setFont(Font.font(14));
+        out.setWrapText(true);
+        bottom.getChildren().addAll(outputText(), out, reset());
+        mainLayout.setBottom(bottom);
+        calculate.setVisible(false);
+        jobBox.setVisible(false);
+    }
+
+    private Label outputText() {
+        Label output = new Label();
+        output.setText("Simulated Job Order: (Job, Burst Time ) ");
+        output.setFont(Font.font(16));
+        return output;
+    }
+
+
+    private Button reset(){
+        Button reset = new Button();
+        reset.setText("reset");
+        reset.setOnAction(event-> setMainWindow());
+        return reset;
+    }
+
     private ComboBox makeDropdown(){
+        times = new ArrayList();
         ObservableList<Integer> NUMBER_JOBS = FXCollections.observableArrayList();
         for(int i =2; i< 21; i++){
             NUMBER_JOBS.add(i);
         }
-        final ComboBox jobBox = new ComboBox(NUMBER_JOBS);
-        jobBox.setOnAction(event-> makeInputs(jobBox.getSelectionModel().getSelectedItem().toString()));
+        jobBox = new ComboBox(NUMBER_JOBS);
+        jobBox.setOnAction(event->{
+            makeInputs(jobBox.getSelectionModel().getSelectedItem().toString());
+            calculateButton().setVisible(true);
+        } );
         return jobBox;
     }
 
-    private void makeInputs(String num){
+    public void makeInputs(String num){
         int jobNum = Integer.parseInt(num);
         VBox inputBoxes = new VBox();
-
         for(int i = 1; i <= jobNum; i++){
             inputBoxes.getChildren().add(makeJobInput(i));
         }
@@ -57,7 +124,7 @@ public class FCFSLayout {
         mainLayout.setCenter(jobScroll);
     }
 
-    private HBox makeJobInput(int num){
+    protected HBox makeJobInput(int num){
         HBox job = new HBox();
         Label jobNum = new Label("Job Number "+ num);
         jobNum.setPadding(new Insets(10,50,0,0));
@@ -67,8 +134,15 @@ public class FCFSLayout {
         job.getChildren().addAll(jobNum, burst);
         job.setStyle("-fx-border-color: black;");
         job.setPadding(new Insets(10,10,10,10));
+        times.add(burst);
         return job;
     }
 
-
+    private Label error(){
+        Label error = new Label();
+        error.setText("Error: input must be number value");
+        error.setTextFill(Color.RED);
+        error.setFont(Font.font(20));
+        return error;
+    }
 }
--- src/edu/bsu/cs376/SJFLayout.java
+++ src/edu/bsu/cs376/SJFLayout.java
@@ -1,7 +1,32 @@
 package edu.bsu.cs376;
 
-/**
- * Created by nicos_000 on 11/15/2016.
- */
-public class SJFLayout {
+import java.util.*;
+
+public class SJFLayout extends FCFSLayout{
+
+    @Override
+    public void simulate(){
+        Map<String, Integer> hm = new HashMap<>();
+        String job;
+        String output = "";
+        for(int i = 0; i < times.size(); i++){
+            job = "Job "+ (i+1) ;
+            hm.put(job, Integer.valueOf(times.get(i).getText()));
+        }
+
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(hm.size());
+        entries.addAll(hm.entrySet());
+        Collections.sort(entries, (entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()));
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String , Integer> entry : entries)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String,Integer> entry : sortedMap.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            output += "  " + key + ", " + value + ", ";
+        }
+        showOutput(output);
+    }
 }
